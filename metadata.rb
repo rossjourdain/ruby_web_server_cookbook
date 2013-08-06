772dfe768f6487100a4873ea1892971be2a69bc0
Ross Jourdain <ross.jourdain@gmail.com>
2013-08-06 20:59:20 +1200

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'ruby_web_server'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures ruby_web_server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: ruby_web_server
+# Recipe:: default
+#
+# Copyright (C) 2013 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
